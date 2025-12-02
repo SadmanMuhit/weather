b2f1dabc7f70edab27c05498f4fe2f002dc7ede0
sadman muhit <ssmuhit9699@gmail.com>
2025-12-02 19:02:25 +0600

    weather report

---
+++ app/action.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { WeatherData } from "./types/weather";
+
+export async function getWeatherData(
+  city: string
+): Promise<{ data?: WeatherData }> {
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY}`
+    );
+    const data = await res.json();
+    return { data };
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+}
