2e43940916070063a2ed0ae59671fecdcc1a45d6
sadman muhit <ssmuhit9699@gmail.com>
2025-12-03 14:38:25 +0600

    complete

--- app/action.ts
+++ app/action.ts
@@ -1,18 +1,52 @@
 "use client";
 
 import { WeatherData } from "./types/weather";
+import { z } from "zod";
+
+const weatherShema = z.object({
+  name:z.string(),
+  main: z.object({
+    temp: z.number(),
+    humidity: z.number(),
+    feels_like: z.number(),
+  }),
+  weather: z.array(
+    z.object({
+      main: z.string(),
+      description: z.string(),
+      icon: z.string(),
+    }),
+  ),
+  wind: z.object({
+    speed: z.number(),
+  }),
+})
 
 export async function getWeatherData(
   city: string
-): Promise<{ data?: WeatherData }> {
+): Promise<{ data?: WeatherData,
+  error?: string
+ }> {
   try {
+    if(!city.trim()){
+      return {error: "city name is required" }
+    }
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY}`
-    );
-    const data = await res.json();
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY}`);
+      if(!res.ok){
+        throw new Error("city not found")
+      }
+    const rawData = await res.json();
+    const data = weatherShema.parse(rawData);
     return { data };
   } catch (error) {
-    console.error(error);
-    return {};
+    if(error instanceof z.ZodError){
+      return {
+        error: "Invalid weather data received"
+      };
+    }
+    return {
+      error: error instanceof Error ? error.message : "Failed to fatch weather data"
+    }
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,41 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Droplets, Search, Thermometer, Wind } from "lucide-react";
 import { getWeatherData } from "./action";
 import { useState } from "react";
 import { WeatherData } from "./types/weather";
 import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
-
+import { useFormStatus } from "react-dom";
+import { motion } from "framer-motion";
 function SubmitButton() {
+  const { pending } = useFormStatus();
   return (
-    <Button type="submit">
-      <Search className="w-4 h-4" />
+    <Button type="submit" disabled={pending}>
+      <Search className={`h-4 w-4 ${pending ? " animate-spin" : ""}`} />
     </Button>
   );
 }
 export default function Home() {
   const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [error, setError] = useState<string>("")
   console.log("weather", weather);
 
   const handleSearch = async (formData: FormData) => {
-    console.log("clicked");
+    setError("");
+
     const city = formData.get("city") as string;
-    const { ...data } = await getWeatherData(city);
-    if (data) {
-      setWeather(data?.data ?? null);
+    const { data, error: weatherError } = await getWeatherData(city);
+    console.log(error);
+    if(weatherError){
+      setError(weatherError);
+      setWeather(null);
     }
-  };
+    
+    if (data) {
+      setWeather(data);
+    }
+  };  
   return (
     <>
       <div className="min-h-screen bg-linear-to-b from-sky-400 to-blue-500 p-4 flex items-center justify-center">
@@ -41,18 +50,52 @@ export default function Home() {
             />
             <SubmitButton />
           </form>
+          
+          {
+            error &&(
+              <motion.div initial={{ opacity:0, y: 10}} animate={{opacity:1, y:0}} exit={{opacity:0}} className="text-center text-red-200 bg-red-500/20 rounded-md p-2 mt-4">{error}</motion.div>
+            )
+            
+            }
+
           {weather && (
-            <div className="mt-">
-              <Card className="bg-white/70 backdrop-blur">
+            <motion.div initial={{ opacity:0, y: 20}} animate={{opacity:1, y:0}} exit={{opacity:0}} transition={{duration: 0.3}} className="mt-5">
+              <Card className="bg-white/50 backdrop-blur">
                 <CardContent className="p-6">
-                  <h2>{weather.name}</h2>
+                  <div className="text-center">
+                    <h2 className="text-3xl font-bold">{weather.name}</h2>
                   <div className="flex justify-center items-center gap-2 mt-5">
-                    <div></div>
-                    <Image src="/cloud.png" width={100} height={100}/>
+                    <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} width={64} height={64} />
+                    <div className="text-5xl font-bold">{Math.round(weather.main.temp)}°C</div>
+                  </div>
+                  <div className="text-gray-500 mt-2 capitalize">{weather.weather[0].description}</div>
+                  </div>
+                  <div className="grid grid-cols-3 gap-4 mt-6">
+                    <div className="text-center">
+                      <Thermometer className="w-6 h-6 mx-auto text-orange-500"/>
+                      <div>Feels like</div>
+                      <div>
+                        {Math.round(weather.main.feels_like)}°C
+                      </div>
+                    </div>
+                    <div className="text-center">
+                      <Droplets className="w-6 h-6 mx-auto text-blue-500"/>
+                      <div>Humidity</div>
+                      <div>
+                        {Math.round(weather.main.humidity)}%
+                      </div>
+                    </div>
+                    <div className="text-center">
+                      <Wind className="w-6 h-6 mx-auto text-teal-500"/>
+                      <div>Feels like</div>
+                      <div>
+                        {Math.round(weather.wind.speed)} m/s
+                      </div>
+                    </div>
                   </div>
                 </CardContent>
               </Card>
-            </div>
+            </motion.div>
           )}
         </div>
       </div>
